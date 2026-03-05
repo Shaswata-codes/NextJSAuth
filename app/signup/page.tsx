f91e4792a38142e6ec7536e16d19920c3499a8b1
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2026-03-05 11:32:55 +0530

    SignUp done

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,23 +1,48 @@
 "use client";
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import {axios} from 'axios';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 export default function SignupPage() {
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email : "",
         password : "",
         username : "",
     });
 
-    const onSignup = async () =>{
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
+    useEffect(()=>{
+        if(user.email.length>0 && user.password.length > 0 && user.username.length > 0){
+            setButtonDisabled(false);
+        }
+        else{
+            setButtonDisabled(true);
+        }
+    }, [user]);
+
+    const onSignup = async () =>{
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            toast.success("Signup Successful");
+            router.push("/login");
+            console.log("User data being sent:", user);
+        } catch (error) {
+            console.log(error);
+            toast.error("Signup Failed");
+        } finally{
+            setLoading(false);
+        }
     }
 
     return (
         <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-            <h1>SignUp</h1>
+            <h1>{loading ? "Processing" : "Signup"}</h1>
             <label htmlFor='username'>username</label>
             <input 
             className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
@@ -26,14 +51,14 @@ export default function SignupPage() {
             <label htmlFor='email'>email</label>
             <input 
             className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
-            type="text" id="email" onChange={(e) => setUser({...user, email : e.target.value})} placeholder='email' />
+            type="text" id="email" value={user.email} onChange={(e) => setUser({...user, email : e.target.value})} placeholder='email' />
 
             <label htmlFor='password'>password</label>
             <input 
             className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
             type="password" id="password" onChange={(e) => setUser({...user, password : e.target.value})} placeholder='password' />
 
-            <button onClick={onSignup} className='p-2 bg-blue-500 text-white rounded-lg cursor-pointer'>Signup</button>
+            <button onClick={onSignup} className='p-2 bg-blue-500 text-white rounded-lg cursor-pointer'>{buttonDisabled ? "No Signup" : "Signup"}</button>
 
             <Link href="/login">Visit login page</Link>
 
--- models/userModel.ts
+++ models/userModel.ts
@@ -1,30 +1,34 @@
-import { verify } from "crypto";
 import mongoose from "mongoose";
-import { m } from "motion/react";
-import { p } from "motion/react-client";
-import { unique } from "next/dist/build/utils";
+
 const userSchema = new mongoose.Schema({
     username:{
-        type : String,
-        required : true,
-        unique  : true,
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
     },
     password:{
-        type : String,
-        required : [true,"Password is required"],
+        type: String,
+        required: true
     },
     isVerified:{
         type: Boolean,
-        default: false,
+        default: false
     },
-    isAdmin :{
-        type:Boolean,
-        default : false,
+    isAdmin:{
+        type: Boolean,
+        default: false
     },
-    forgotPasswordToken : String,
-    forgotPasswordExpiry : Date,
-    verifyToken : String,
-    verifyTokenExpiry : Date,
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date
 });
 
-const User = mongoose.model("users", userSchema);
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
